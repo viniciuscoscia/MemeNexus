3eaf5aa135c5971ac9f42c5b9934f25dfde058c4
Vinicius Coscia <vinicius.coscia@live.com>
2024-12-06 00:52:00 -0300

    Displaying the SVG in the Home Screen. Finally!

--- composeApp/src/commonMain/kotlin/com/viniciuscoscia/memenexus/meme/presentation/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/viniciuscoscia/memenexus/meme/presentation/home/HomeScreen.kt
@@ -2,43 +2,40 @@ package com.viniciuscoscia.memenexus.meme.presentation.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.viniciuscoscia.memenexus.core.presentation.DarkContent
 import com.viniciuscoscia.memenexus.core.presentation.DarkSurface
 import com.viniciuscoscia.memenexus.core.presentation.MemeNexusTheme
 import com.viniciuscoscia.memenexus.core.presentation.components.DropdownMenuWithArrow
-import org.jetbrains.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import coil3.ImageLoader
+import coil3.compose.AsyncImage
+import coil3.compose.AsyncImagePainter
+import coil3.compose.LocalPlatformContext
+import coil3.compose.rememberAsyncImagePainter
+import coil3.request.ImageRequest
+import coil3.svg.SvgDecoder
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun HomeScreenRoot() {
@@ -56,30 +53,58 @@ fun HomeScreen() {
             .statusBarsPadding(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            HomeTopBar()
-
-        }
+        HomeTopBar()
+        HomeBody()
     }
 }
 
 @Composable
 private fun HomeTopBar() {
     Row(
-        modifier = Modifier
-            .background(DarkContent)
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
+        modifier = Modifier.fillMaxWidth()
     ) {
-        Title()
-        val options = listOf("Favorites First", "Newest First")
-        DropdownMenuWithArrow(
-            options = options
-        )
+        Row(
+            modifier = Modifier
+                .background(DarkContent)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Title()
+            val options = listOf("Favorites First", "Newest First")
+            DropdownMenuWithArrow(
+                options = options
+            )
+        }
     }
+}
+
+@Composable
+fun ColumnScope.HomeBody() {
+    Surface(
+        modifier = Modifier
+            .weight(1f)
+            .fillMaxWidth(),
+        color = MaterialTheme.colorScheme.surfaceContainer
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            RemoteSvgLoader3(svgPath = "https://meme-nexus.s3.us-east-2.amazonaws.com/AppSVGs/homeBackgroundEmptyList.svg")
+        }
+    }
+}
+
+@Composable
+fun RemoteSvgLoader3(svgPath: String) {
+    AsyncImage(
+        model = ImageRequest.Builder(LocalPlatformContext.current)
+            .data(svgPath)
+            .decoderFactory(SvgDecoder.Factory())
+            .build(),
+        contentDescription = "Empty List Background",
+        modifier = Modifier.fillMaxSize()
+    )
 }
 
 @Composable
