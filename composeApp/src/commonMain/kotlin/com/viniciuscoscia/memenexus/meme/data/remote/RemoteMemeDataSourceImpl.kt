dd46fa0e2c57fb7c14c639f40f8bb804e84ec187
Vinicius Coscia <vinicius.coscia@live.com>
2024-12-14 17:54:08 -0300

    COde cleaning

--- composeApp/src/commonMain/kotlin/com/viniciuscoscia/memenexus/meme/data/remote/RemoteMemeDataSourceImpl.kt
+++ composeApp/src/commonMain/kotlin/com/viniciuscoscia/memenexus/meme/data/remote/RemoteMemeDataSourceImpl.kt
@@ -7,16 +7,7 @@ import io.ktor.client.HttpClient
 class RemoteMemeDataSourceImpl(
     private val httpClient: HttpClient
 ) : RemoteMemeDataSource {
-    //    safeCall<SearchResponseDto> {
-//        httpClient.get(
-//            urlString = "$BASE_URL/search.json"
-//        ) {
-//            parameter("q", query)
-//            parameter("limit", resultLimit)
-//            parameter("language", "eng")
-//            parameter("fields", "key,title,author_name,author_key,cover_edition_key,cover_i,ratings_average,ratings_count,first_publish_year,language,number_of_pages_median,edition_count")
-//        }
-//    }
+
     override suspend fun fetchMemes(): Result<Any, DataError.Remote> {
         return Result.Success(data = Any())
     }
